1c8763426db54e531e7bc8f112d23c0287133c9b
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-06 23:46:52 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,3 +5,15 @@
 #include <LiquidCrystal_I2C.h>
 
 float Vo = 0;
+float Voc = 0;
+float DD = 0;
+float AvgDD = 0;
+float SumDD = 0;
+int count = 0;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void setup() {
+    
+}
